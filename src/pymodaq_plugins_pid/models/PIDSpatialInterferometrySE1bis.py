192a3ff7155d570c534addab13c7d2748bea866d
MatthieuGuer <matthieu.guer@free.fr>
2023-03-20 10:08:18 +0100

    Trying to add a recorder, not working yet

--- src/pymodaq_plugins_pid/models/PIDSpatialInterferometrySE1bis.py
+++ src/pymodaq_plugins_pid/models/PIDSpatialInterferometrySE1bis.py
@@ -5,6 +5,7 @@ from pyqtgraph.dockarea import Dock
 import scipy.signal.windows as windows
 import scipy.fft as fft
 from collections import deque
+from datetime import datetime
 
 from pyqtgraph.Qt import QtCore
 import pyqtgraph as pg
@@ -16,6 +17,7 @@ from pymodaq.daq_utils.plotting.viewer1D.viewer1D_main import Viewer1D
 from pymodaq.daq_utils.daq_utils import Axis
 from pymodaq.daq_utils.daq_utils import   DataFromPlugins
 from pymodaq.daq_utils.math_utils import ft,ift
+from pymodaq.daq_utils.h5modules import H5Saver
 
 
 
@@ -31,15 +33,21 @@ class PIDModelSpetralInterferometrySE1bis(PIDModelGeneric):
     detectors_name = ['Camera PID']
 
     params = [
-        {'title': 'Wavelength (nm)','name' : 'wavelength','type':'float','value' : 800},
-        {'title': 'Actuator unit (m)','name' : 'unit','type':'float','value' : 1e-6},
-        {'title': 'RMS','name' : 'RMS','type':'float','value' : 0.0, 'readonly': True},
+        {'title': 'Wavelength (nm)','name': 'wavelength','type':'float','value' : 800},
         {'title': 'Units', 'name': 'unitsGroup', 'type':'group', 'expanded':True, 'visible':True,
         'children': [
             {'title': 'Convert to femto', 'name': 'convertFemto', 'type':'bool', 'value': True},
             {'title': 'Actuator unit (m)', 'name': 'actUnits', 'type':'float', 'value': 1e-6},
             {'title': 'Set delay to zero', 'name': 'setDelayToZero', 'type':'bool_push', 'value': False}
         ]},
+        {'title': 'Stats', 'name': 'statsGroup', 'type':'group', 'expanded':True, 'visible':True,
+        'children': [
+            {'title': 'RMS','name': 'RMS','type':'float','value' : 0.0, 'readonly': True},
+            {'title': 'Record delays', 'name': 'record', 'type':'bool_push', 'value': False},
+            {'title': 'Stop recording', 'name': 'recordStop', 'type':'bool_push', 'value': False, 'visible':False},
+            {'title': 'Number of delays', 'name': 'N_record', 'type':'int', 'value': 1e3},
+        ]},
+
         {'title': 'Spectrum ROI', 'name': 'spectrum', 'type': 'group', 'expanded': False, 'visible': True,
          'children': [
             {'title': 'Omega min', 'name': 'omega_min', 'type': 'float', 'value':0.0 },
@@ -66,8 +74,7 @@ class PIDModelSpetralInterferometrySE1bis(PIDModelGeneric):
         super().__init__(pid_controller)
 
         self.wavelength = self.settings.child('wavelength').value()
-        self.phase_vector = deque(maxlen=100)
-        self.offset = 0
+        self.recording = False
 
         self.d1 = Dock("Camera data")
         widget_calib = QtWidgets.QWidget()
@@ -79,7 +86,7 @@ class PIDModelSpetralInterferometrySE1bis(PIDModelGeneric):
         w1.addItem(self.img1a)
         self.d1.addWidget(w1)
 
-        self.roi = pg.RectROI([20,20],[20,20],pen=(5,30))
+        self.roi = pg.RectROI([20,20],[100,100],pen=(5,30))
         self.roi.addRotateHandle([1,0],[0.5,0.5])
         w1.addItem(self.roi)
 
@@ -168,15 +175,48 @@ class PIDModelSpetralInterferometrySE1bis(PIDModelGeneric):
         if param.name() == 'setDelayToZero':
             self.offset = self.phi
             print('The offset phase is now: ', self.offset)
-        # if param.name() == 'unit':
-            # self.unit = param.value()
-            # print('Actuator unit changed to: ',  self.unit)
+        if param.name() == 'record':
+            self.init_saver()
+        if param.name() == 'recordStop':
+            self.close_saver()
 
-            
+
+
+    def init_saver(self):
+        self.timearray = np.zeros((self.settings.child('statsGroup', 'N_record').value(), 3))
+        self.delayarray = np.zeros(self.settings.child('statsGroup', 'N_record').value())
+        self.Nrecorded = 0
+        self.recording = True
+        self.h5_saver = H5Saver()
+        time_init = datetime.now()
+        self.h5_saver.init_file(custom_naming=False, addhoc_file_path='C:/Users/mguer/Matthieu/recording_{0.year}{0.month}{0.day}_{0.hour}h{0.minute}.h5'.format(time_init))
+        
+
+        self.h5_saver.add_data_group('/', title='Time', group_data_type='data1D')
+        self.h5_timegroup = self.h5_saver.current_group
+        self.h5_saver.add_data_group('/', title='Delay', group_data_type='data1D')
+        self.h5_delaygroup = self.h5_saver.current_group
+
+
+        self.settings.child('statsGroup', 'record').setOpts(visible=False)
+        self.settings.child('statsGroup', 'recordStop').setOpts(visible=True)
+        print('saver_inited')
+
+    def close_saver(self):
+            self.recording = False
+            self.settings.child('statsGroup', 'record').setOpts(visible=True)
+            self.settings.child('statsGroup', 'recordStop').setOpts(visible=False)
+            self.h5_saver.close()
+
+
 
     def ini_model(self):
         super().ini_model()
-    
+
+        self.phase_vector = deque(maxlen=100)
+        self.offset = 0
+
+
     def convert_input(self, measurements):
         """
         Convert the image of the camera into x and y positions of the center of the beam.
@@ -220,16 +260,36 @@ class PIDModelSpetralInterferometrySE1bis(PIDModelGeneric):
             self.plotph.setData(y = np.angle(np.exp(np.array(self.phase_vector)*1j)))
 
         rms = np.std(self.phase_vector)
-        self.settings.child('RMS').setValue(rms)
+        self.settings.child('statsGroup', 'RMS').setValue(rms)
+
+
 
         if self.settings.child('unitsGroup', 'convertFemto').value():
-            delay = (self.phi-self.offset) * self.wavelength * 1e-9 * 2*np.pi / 3e8 / 1e-15
-            return InputFromDetector([delay])
+            delay = (self.phi-self.offset) * self.wavelength * 1e-9 / (2*np.pi * 3e8) * 1e-15
         else:
-            return InputFromDetector([(self.phi-self.offset)/np.pi*180])
+            delay = (self.phi-self.offset)/np.pi*180
+
+        if self.recording:
+            print('trying to save')
+            if self.Nrecorded < self.settings.child('statsGroup', 'N_record').value():
+                print('writing in array')
+                current_time = datetime.now().time()
+                self.timearray[self.Nrecorded, 0] = current_time.hour
+                self.timearray[self.Nrecorded, 1] = current_time.minute
+                self.timearray[self.Nrecorded, 2] = current_time.second
+                self.delayarray[self.Nrecorded] = delay
+                self.Nrecorded += 1
+            # if self.Nrecorded == self.settings.child('statsGroup', 'N_record'):
+            else:
+                self.h5_saver.add_data(self.h5_timegroup, data_dict=dict(data=self.timearray), title='time')
+                self.h5_saver.add_data(self.h5_delaygroup, data_dict=dict(data=self.delayarray), tile='delay')
+                self.recording=False
+                self.close_saver()
+
+        return InputFromDetector([delay])
 
 
-    def convert_output(self, outputs, dt, stab=True):
+    def convert_output(self, outputs, dt=0, stab=True):
         """
         Convert the output of the PID in units to be fed into the actuator
         Parameters
@@ -247,7 +307,7 @@ class PIDModelSpetralInterferometrySE1bis(PIDModelGeneric):
             self.curr_output = np.zeros(np.shape(outputs))
         else:
             if self.settings.child('unitsGroup', 'convertFemto').value():
-                self.curr_output = np.array(outputs) * 3e8 * 1e-15 /2 / self.settings.child('unitsGroup', 'actUnits').value()
+                self.curr_output = np.array(outputs) * 3e8 * 1e-15 / self.settings.child('unitsGroup', 'actUnits').value() /2
             else:
                 self.curr_output = np.array(outputs) /360 * self.wavelength * 1e-9 /2 / self.settings.child('unitsGroup', 'actUnits').value()
 
